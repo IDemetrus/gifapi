c1beec23bbf46b19f72f821a427e68590f5ac541
IDemetrus <workhome.pro@gmail.com>
2021-02-18 17:49:40 +0300

    added fragment for tabs

---
+++ app/src/main/java/com/example/gifapp/ui/TabsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.gifapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+private const val TAG = "TabsFragment"
+
+class TabsFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
